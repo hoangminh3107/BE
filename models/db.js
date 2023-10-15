ef7078d4a98b423dce847f510bd44d2ee962a6c6
BachToan <toanbui765@gmail.com>
2023-10-15 13:53:35 +0700

    route cho api

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://dath33603:<password>@cluster0.xcezadi.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://dath33603:QWCNouzimMLmQESf@cluster0.xcezadi.mongodb.net/?retryWrites=true&w=majority')
        .catch( (err)=>{
                console.log("Loi ket noi CSDL: ");
                console.log(err);
