e21eb5a02ddb93dd4436fa44d6c4b37e074773e5
datha <dath33603@gmail.com>
2023-11-06 20:29:39 +0700

    fix lai model product

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
-router.get('/home', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+router.get("/addProduct", function (req, res, next) {
+  res.render("product/addProduct", { title: "Express" });
+});
+router.get("/home", function (req, res, next) {
+  res.render("home", { title: "Express" });
 });
 
 module.exports = router;
