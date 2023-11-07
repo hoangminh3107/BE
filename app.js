f7fdf9c8bb953cc409c2aa014bcc097f96dc7f33
datha <dath33603@gmail.com>
2023-11-07 21:50:45 +0700

    day code phan addProduct

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var bodyParser = require("body-parser");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var apiRouter = require("./routes/api");
@@ -13,13 +13,14 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,48 +1,61 @@
 var productModel = require("../models/product.model");
-
-
+const fs = require("fs");
 exports.getSuggest = async (req, res, next) => {
-    try {
-      let list = await productModel.productModel.find();
-      if (list) {
-        return res
-          .status(200)
-          .json({ data: list, msg: "Lấy dữ liệu thành công" });
-      } else {
-        return res.status(400).json({ msg: "Không có dữ liệu" });
-      }
-    } catch (error) {
-      return res.status(500).json({ msg: error.message });
+  try {
+    let list = await productModel.productModel.find();
+    if (list) {
+      return res
+        .status(200)
+        .json({ data: list, msg: "Lấy dữ liệu thành công" });
+    } else {
+      return res.status(400).json({ msg: "Không có dữ liệu" });
     }
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
 };
 
-
 exports.getProduct = async (req, res, next) => {
-    
-    try {
-        const product = await productModel.productModel.findById(req.params.id);
-      
-        if (!product) {
-          return res.status(204).json({ msg: 'Sản phẩm không tồn tại' });
-        }
-      
-        res.status(200).json(product);
-      } catch (error) {
-        return res.status(204).json({ msg: error.message });
-      }
+  try {
+    const product = await productModel.productModel.findById(req.params.id);
+
+    if (!product) {
+      return res.status(204).json({ msg: "Sản phẩm không tồn tại" });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    return res.status(204).json({ msg: error.message });
+  }
 };
 exports.getProductByName = async (req, res, next) => {
   const productName = req.body.name;
-  console.log(productName);
   try {
-    const products = await productModel.productModel.find({ name: { $regex: productName, $options: 'i' } });
+    const products = await productModel.productModel.find({
+      name: { $regex: productName, $options: "i" },
+    });
 
     if (products.length === 0) {
-      return res.status(404).json({ msg: 'Không tìm thấy sản phẩm nào.' });
+      return res.status(404).json({ msg: "Không tìm thấy sản phẩm nào." });
     }
 
     res.status(200).json(products);
   } catch (error) {
     return res.status(500).json({ msg: error.message });
   }
+};
+
+exports.addProduct = async (req, res, next) => {
+  const imageFilePath = req.body.image;
+
+  fs.readFile(imageFilePath, (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // Dữ liệu ảnh sẽ nằm trong biến 'data' dưới dạng buffer.
+      const base64Image = data.toString("base64");
+      console.log(base64Image);
+      // Bây giờ bạn có thể sử dụng biến 'base64Image' trong ứng dụng của bạn.
+    }
+  });
 };
